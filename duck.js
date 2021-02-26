963b19a7fbf73778dc47ddc1d21be408cbc3fbc7
Thomas Steven <thomaspatricksteven@gmail.com>
2021-02-25 20:22:44 -0500

    duck says things

--- duck.js
+++ duck.js
@@ -2,6 +2,16 @@
 
 const duck = document.getElementById("duck");
 
+const thingsDuckCouldSay = [
+  "Interesting...",
+  "Go on...",
+  "Hmmm",
+  "I see what you are saying...",
+  "Feed me bread",
+];
+
 duck.addEventListener("click", function () {
-  alert("quack");
+  const index = Math.floor(Math.random() * thingsDuckCouldSay.length);
+  const saying = thingsDuckCouldSay[index];
+  alert(saying);
 });
