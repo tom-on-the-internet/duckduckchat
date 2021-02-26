2f9c00c2a6c216976b189d855f232512b20e5246
Thomas Steven <thomaspatricksteven@gmail.com>
2021-02-25 20:32:13 -0500

    rust

--- duck.js
+++ duck.js
@@ -10,6 +10,9 @@ const thingsDuckCouldSay = [
   "Hmmm",
   "I see what you are saying...",
   "Feed me bread",
+  "I'm not following you there.",
+  "Can you back up a step",
+  "Have you tried rewriting it in Rust?",
 ];
 
 duck.addEventListener("click", function () {
