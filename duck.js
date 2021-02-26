728b0b2d9253452a29d319fa06de7af5a9b4a45c
Thomas Steven <thomaspatricksteven@gmail.com>
2021-02-25 21:24:26 -0500

    add wisdom

--- duck.js
+++ duck.js
@@ -7,12 +7,16 @@ const duck = document.getElementById("duck");
 const thingsDuckCouldSay = [
   "Interesting...",
   "Go on...",
-  "Hmmm",
+  "Hmm...",
   "I see what you are saying...",
   "Feed me bread",
   "I'm not following you there.",
-  "Can you back up a step",
+  "Can you back up a step?",
   "Have you tried rewriting it in Rust?",
+  "Quack",
+  "Is there a simpler way of doing that?",
+  "That makes sense to me.",
+  "Let's break this down into smaller pieces.",
 ];
 
 duck.addEventListener("click", function () {
